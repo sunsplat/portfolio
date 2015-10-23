0df9551704c526e0e821b15b9b49492a9c99ca48
sunsplat <heeycutie@yahoo.com>
2015-10-23 08:42:03 +0800

    created view

--- application/views/pages/quotes.php
+++ application/views/pages/quotes.php
@@ -9,11 +9,28 @@
         <div class="main">
                 <?php echo $quotes_item['author']; ?>
         </div>
-        <p><a href="<?php echo site_url('quotes/'.$quotes_item['slug']); ?>">View more quotes.</a></p>
 
 			<?php endforeach; ?>
+			<div class="col-md-5">
+				<?php echo validation_errors(); ?>
+
+				<?php echo form_open('quotes/create'); ?>
+					<div class="form-group">
+						<label for="quote">Quote</label>
+				    <textarea name="quote"></textarea><br />
+				  </div>
+				  <div class="form-group">
+				    <label for="author">Author</label>
+				    <input type="input" name="author" /><br />
+				  </div>
+				  <div class="form-group">
+			    	<input type="submit" name="submit" value="Add new quote" />
+			    </div>
+				</form>
+			</div>
 		</div>
 	</div>
 <div>
 
+        <p><a href="<?php echo site_url('quotes/'.$quotes_item['slug']); ?>">View more quotes.</a></p>
 
