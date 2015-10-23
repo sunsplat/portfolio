566d2d3002615243f70896ff2e180805474f5e4e
sunsplat <heeycutie@yahoo.com>
2015-10-24 02:15:16 +0800

    modified button style and link

--- application/views/pages/quotes.php
+++ application/views/pages/quotes.php
@@ -2,42 +2,34 @@
 	<div class="row quotes">
 		<div class="col-md-6 col-md-offset-3">
 			<h1>Quotes</h1>
+			<h3>"<?php echo $quotes[0]['quote']; ?>"</h3>
+				<p><?php echo $quotes[0]['author']; ?></p>
 
-			<?php foreach ($quotes as $quotes_item): ?>
+			<!-- <?php foreach ($quotes as $quotes_item): ?>
 
-        <h3><?php echo $quotes_item['quote']; ?></h3>
-        <div class="main">
-                <?php echo $quotes_item['author']; ?>
-        </div>
+         <h3><?php echo $quotes_item['quote']; ?></h3>
+         <div class="main">
+                 <?php echo $quotes_item['author']; ?>
+         </div>
 
-			<?php endforeach; ?>
-
-			<div class="col-md-5">
-
-				<?php echo validation_errors(); ?>
-
-				<?php echo form_open('quotes/create'); ?>
-					<div class="form-group">
-						<label for="quote">Quote</label>
-				    <textarea name="quote"></textarea><br />
-				  </div>
-				  <div class="form-group">
-				    <label for="author">Author</label>
-				    <input type="input" name="author" /><br />
-				  </div>
-				  <div class="form-group">
-			    	<input type="submit" name="submit" value="Add new quote" />
-			    </div>
-
-				</form>
-
-			</div>
+			 <?php endforeach; ?> -->
+			
 		</div>
 	</div> <!-- row quotes -->
+	<div class="center">
+		<a href="<?php echo site_url('pages/quotes'); ?>" class="quotebutton">View more quotes.</a></p>
+	</div>
 <div> <!-- container -->
 
-<div class="container">
-        <p><a href="<?php echo site_url('quotes/'.$quotes_item['slug']); ?>">View more quotes.</a></p>
-</div>
 
+<script>
+	
+
+	$('button').click(function() {
+		var url = "quotes/callme" // the url to the controller and method
+		$.get({
+			url: url
+		})
+	})
+</script>
 
