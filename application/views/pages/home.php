5d376f9a43c2a6fed927be0eafc91e0dd0806445
sunsplat <heeycutie@yahoo.com>
2015-10-24 02:38:08 +0800

    added about link

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -63,32 +63,13 @@
 
 <?php $this->load->view("templates/snippets/carousel.php"); ?>
 
-<a name="portfolio">
-<?php $this->load->view("templates/snippets/thumbnails.php"); ?>
-</a>
+<div id="about">
+  <?php $this->load->view("pages/about.php"); ?>
+</div>
 
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('input#submit').click(function(){
-            var data = $('form').serialize();
-            $.ajax({
-                method: 'POST',
-                cache: false,
-                url: 'pages/create',
-                datatype: 'json',
-                data: data,
-                success: function(data){
-                    alert('yay it worked');
-                    $('#myModal').modal('hide');
-                    $('input:text, textarea').val('');
-                },
-                failure: function(){
-                    console.log('It didnt work');
-                }
-            });
-          
-        });
-    });
-</script>
+<div name="portfolio">
+  <?php $this->load->view("templates/snippets/thumbnails.php"); ?>
+</div>
+
 
 		
