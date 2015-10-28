f90c62d5ae124d41b910846297bd10d70bb7cfc4
sunsplat <heeycutie@yahoo.com>
2015-10-28 11:23:02 +0800

    moved js to footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,13 @@
-<div class="container">
+<div class="container footer">
 	<footer class="footer">
         	<em>&copy; 2015</em> Ellen Sun | Last Updated: <?php $date = new DateTime();
               echo date_format($date, 'Y M'); ?>
   </footer>
 </div>
-        
+
+        <script src="<?php echo base_url('assets/bootstrap/js/jquery-1.11.3.min.js'); ?>"></script>	
+        <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+	  		<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
         <script src="<?php echo base_url('assets/bootstrap/js/pagescroll/jquery.easing.min.js'); ?>"></script>
         <script src="<?php echo base_url('assets/bootstrap/js/pagescroll/scrolling-nav.js'); ?>"></script>
   </body>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,9 +5,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
-	  <script src="<?php echo base_url('assets/bootstrap/js/jquery-1.11.3.min.js'); ?>"></script>	
-	  <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
-		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
+	 		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
     
     <title><?php echo ucwords($title); ?></title>
 
