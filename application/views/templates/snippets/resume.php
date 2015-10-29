019719d37037acd5b15908f5d89226f571a5ec29
sunsplat <heeycutie@yahoo.com>
2015-10-29 10:25:01 +0800

    put education and skills after work experience

--- application/views/templates/snippets/resume.php
+++ application/views/templates/snippets/resume.php
@@ -1,22 +1,78 @@
 <div class="container">
 	<div class ="row">
+		<div class="col-md-3 col-md-offset-4 title">
 				<h1>My Resume</h1>
+		</div>
 	</div>
 
-	<div class="row">
-		<div class="col-md-4">
-				<h2 class="center"><strong>Education & Skills</strong></h2>
-		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<h2 class="center"><strong>Work Experience</strong></h2>
+			</div>
+			<div class="col-md-6 col-md-offset-1">
+				<h3><strong>LAMP Camp | Developer</strong></h3>
+				<p><em>Building enterprise-level applications with PHP, MySQL, Javascript and much more.</em>
+					<ul>
+						<li>Developed the best possible application solutions for ongoing business needs</li>
+						<li>Used JIRA to plan, track, and release great software while solving challenges and building applications with fellow developers</li> 
+						<li>Deployed projects using Git control in a collaborative development environment; tested applications and code with the latest methods and standards</li>
+						<li>Used an MVC framework to build and release enterprise-level applications in a timely and efficient fashion</li>
+						<li>Applied Agile project management principles and operated in a full Scrum shop</li>
+					</ul>
+				</p>
 
-		<div class="col-md-6 col-md-offset-1">
-				<h3>Georgia Institute of Technology</h3>
+				<h3><strong>Kingsoft | Product Manager</strong></h3>
+				<p><em>Improving Battery Doctor and Clean Master for the international market.</em>
+					<ul>
+						<li>Assisted HR in foreign recruitment, translation of important notices into English, and creating a more international work environment</li>
+						<li>Managed customer feedback and social media (G+, Facebook, Twitter)</li>
+						<li>Increased the Google Play star rating from 4.5 to 4.52 in two weeks</li>
+						<li>Increased Facebook ‘Likes’ from 7,000 to over 150,000 in 2 months</li>
+						<li>Acted as liaison between users/developers/designers/testers to improve UX</li>
+						<li>Increased US CTR (click-thru-rate) from 2.6% to 3.7% in 3 weeks</li> 
+						<li>Conducted user experience surveys and product usability tests</li>
+					</ul>
+				</p>
+
+				<h3><strong>Pactera | Quality Assurance Manager</strong></h3>
+					<p><em>Improving quality of language transcription and annotation projects leading to our 2012 award for most profitable department</em></p>
+					<ul>
+						<li>Was responsible for department training of new hires in domestic and international offices</li>
+						<li>Screened potential foreign applicants for language ability</li>
+					  <li>Interfaced with client and international offices to maintain quality consistency</li>
+					  <li>Improved business relations leading to customer loyalty and a $1.3 million contract</li>
+						<li>Improved voice recognition interface systems by increasing efficiency</li>
+					  <li>Created the first English Employee Handbook as well as the first English department training manual for full-time international employees</li>
+					 </ul>
+
+					<h3><strong>FRS Human Resource LTD | Career Consultant</strong></h3>
+					<p><em>Recruiting international professionals to expand their global business knowledge and experience in China.</em></p>
+					<ul>
+						<li>Generated job descriptions for multiple cases simultaneously</li>
+					  <li>Built strong relationships with Chinese clients through various media that lead to an increase of 10,000 RMB per month in sales</li>
+						<li>Created the first company employee training manual</li>
+					  <li>Drafted legal agreements and documents pertaining to contracts with candidates and clients</li>
+					  <li>Created and managed social media marketing on Linkedin.com, leading to an increase in company awareness and exposure</li>
+					  <li>Translated Chinese/English during important client-candidate face-to-face meetings</li>
+					</ul>
+					
+			</div>
+		</div>
+				
+		<div class="row">
+			<div class="col-md-4">
+				<h2 class="center"><strong>Education & Skills</strong></h2>
+			</div>
+
+			<div class="col-md-6 col-md-offset-1">
+				<h3><strong>Georgia Institute of Technology</strong></h3>
 				<p>Bachelor of Science in Business Management & Marketing
 				</p>
 				<p>Online coursework:
 				Codecademy, General Assembly, Codermanual, Team Treehouse, Railstutorial.org, Code School</p>
 
-		</div>
-		<div class="col-md-3 col-md-offset-5">
+			</div>
+			<div class="col-md-3 col-md-offset-5">
 				<h4>Technical Languages:</h4>
 				<ul>
 					<li>HTML & CSS</li>
@@ -36,9 +92,9 @@
 					<li>English: Native</li>
 					<li>Mandarin Chinese: Working professional</li>
 				</ul>
-		</div>
+			</div>
 		
-		<div class="col-md-3">
+			<div class="col-md-3">
 				<h4>Technical Toolbox:</h4>
 				<ul>
 					<li>Microsoft Office Suite</li>
@@ -59,47 +115,5 @@
 				</ul>
 			</div>
 		</div>
-
-		<div class="row">
-			<div class="col-md-4">
-				<h2 class="center"><strong>Work Experience</strong></h2>
-			</div>
-			<div class="col-md-6 col-md-offset-1">
-				<h3>Kingsoft | Product Manager</h3>
-				<p><em>Improving Battery Doctor and Clean Master for the international market.</em>
-					<ul>
-						<li>Assisted HR in foreign recruitment, translation of important notices into English, and creating a more international work environment</li>
-						<li>Managed customer feedback and social media (G+, Facebook, Twitter)</li>
-						<li>Increased the Google Play star rating from 4.5 to 4.52 in two weeks</li>
-						<li>Increased Facebook ‘Likes’ from 7,000 to over 150,000 in 2 months</li>
-						<li>Acted as liaison between users/developers/designers/testers to improve UX</li>
-						<li>Increased US CTR (click-thru-rate) from 2.6% to 3.7% in 3 weeks</li> 
-						<li>Conducted user experience surveys and product usability tests</li>
-					</ul>
-				</p>
-
-				<h3>Pactera | Quality Assurance Manager</h3>
-					<p><em>Improving quality of language transcription and annotation projects leading to our 2012 award for most profitable department</em></p>
-					<ul>
-						<li>Was responsible for department training of new hires in domestic and international offices</li>
-						<li>Screened potential foreign applicants for language ability</li>
-					  <li>Interfaced with client and international offices to maintain quality consistency</li>
-					  <li>Improved business relations leading to customer loyalty and a $1.3 million contract</li>
-						<li>Improved voice recognition interface systems by increasing efficiency</li>
-					  <li>Created the first English Employee Handbook as well as the first English department training manual for full-time international employees</li>
-					 </ul>
-
-					<h3>FRS Human Resource LTD | Career Consultant</h3>
-					<p><em>Recruiting international professionals to expand their global business knowledge and experience in China.</em></p>
-					<ul>
-						<li>Generated job descriptions for multiple cases simultaneously</li>
-					  <li>Built strong relationships with Chinese clients through various media that lead to an increase of 10,000 RMB per month in sales</li>
-						<li>Created the first company employee training manual</li>
-					  <li>Drafted legal agreements and documents pertaining to contracts with candidates and clients</li>
-					  <li>Created and managed social media marketing on Linkedin.com, leading to an increase in company awareness and exposure</li>
-					  <li>Translated Chinese/English during important client-candidate face-to-face meetings</li>
-					</ul>
-					
-			</div>
-		</div>
-	</div>	
+	</div>
+	
