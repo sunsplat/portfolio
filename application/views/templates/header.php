24d32b2adb7d1dd558de18df602e9e2e2a02a474
sunsplat <heeycutie@yahoo.com>
2015-11-01 04:28:40 +0800

    added base url

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
 	 		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
-    
+    <base href="https://grim-plague-5208.herokuapp.com/">
     <title><?php echo ucwords($title); ?></title>
 
   </head>
