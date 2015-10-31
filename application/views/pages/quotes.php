0f3e3df449c5c9d1afac7ee24cb332f37aef8da9
sunsplat <heeycutie@yahoo.com>
2015-11-01 02:37:15 +0800

    randomized quotes

--- application/views/pages/quotes.php
+++ application/views/pages/quotes.php
@@ -3,17 +3,10 @@
 		<div class="row quotes">
 			<div class="col-md-6 col-md-offset-3">
 				<h1>Quotes</h1>
+				<h3><?php shuffle($quotes); ?></h3>
+
 				<h3>"<?php echo $quotes[0]['quote']; ?>"</h3>
 					<p><?php echo $quotes[0]['author']; ?></p>
-
-				<!-- <?php foreach ($quotes as $quotes_item): ?>
-
-	         <h3><?php echo $quotes_item['quote']; ?></h3>
-	         <div class="main">
-	                 <?php echo $quotes_item['author']; ?>
-	         </div>
-
-				 <?php endforeach; ?> -->
 				
 			</div>
 		</div> <!-- row quotes -->
