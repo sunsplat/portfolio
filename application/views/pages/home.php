67e2520fa30eea188e5319ca9433e886b4aeedf0
sunsplat <heeycutie@yahoo.com>
2015-10-31 12:37:07 +0800

    removed carousel. inserted parallax

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,18 +1,29 @@
-<div id="carousel">
-	<?php $this->load->view("templates/snippets/carousel.php"); ?>
+<div class="wrapper">
+		<section class="module parallax parallax-1">
+			      <div class="container center social">
+		          <a href="http://www.twitter.com/ms_ellerz"><i class="fa fa-twitter fa-5x"></i></a>
+		          <a href="http://www.github.com/sunsplat"><i class="fa fa-github fa-5x"></i></a>
+		          <a href="http://www.linkedin.com/in/msellensun"><i class="fa fa-linkedin fa-5x"></i></a>
+		        </div>
+		</section>
+
+		<section id="about">
+		  <?php $this->load->view("templates/snippets/about.php"); ?>
+		</section>
+
+		<section id="resume"> 
+			<?php $this->load->view("templates/snippets/resume.php"); ?>
+		</section>
+
+		<section id="portfolio">
+		  <?php $this->load->view("templates/snippets/thumbnails.php"); ?>
+		</section>
 </div>
 
-<section id="about">
-  <?php $this->load->view("templates/snippets/about.php"); ?>
-</section>
-
-<section id="resume"> 
-	<?php $this->load->view("templates/snippets/resume.php"); ?>
-</section>
-
-<section id="portfolio">
-  <?php $this->load->view("templates/snippets/thumbnails.php"); ?>
-</section>
+<!-- <div class="image1">
+      <img src="<?php echo base_url('assets/images/carousel-1.jpg'); ?>" class="img-responsive">
+      <div class="carousel-caption">
+         -->
 
 
 
