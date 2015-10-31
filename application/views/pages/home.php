52571d4204cbe85396f78135e91d574a55673d27
sunsplat <heeycutie@yahoo.com>
2015-10-31 11:10:58 +0800

    changed sections to divs and deleted comments

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,43 +1,19 @@
-<div id="carousel">
+<div class="section" id="carousel">
 	<?php $this->load->view("templates/snippets/carousel.php"); ?>
 </div>
 
-<div id="about">
+<section id="about">
   <?php $this->load->view("templates/snippets/about.php"); ?>
-</div>
+</section>
 
-<div id="portfolio">
-  <?php $this->load->view("templates/snippets/thumbnails.php"); ?>
-</div>
-
-<div id="resume"> 
+<section id="resume"> 
 	<?php $this->load->view("templates/snippets/resume.php"); ?>
-</div>
+</section>
 
+<section id="portfolio">
+  <?php $this->load->view("templates/snippets/thumbnails.php"); ?>
+</section>
 
-<!-- <script type="text/javascript">
-//     $(document).ready(function(){
-//         $('input#submit').click(function(){
-//             var data = $('form').serialize();
-//             $.ajax({
-//                 method: 'POST',
-//                 cache: false,
-//                 url: 'pages/create',
-//                 datatype: 'json',
-//                 data: data,
-//                 success: function(data){
-//                     alert('yay it worked');
-//                     $('#myModal').modal('hide');
-//                     $('input:text, textarea').val('');
-//                 },
-//                 failure: function(){
-//                     console.log('It didnt work');
-//                 }
-//             });
-          
-//         });
-//     });
-// </script> -->
 
 
 		
