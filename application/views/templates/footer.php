6961b518d829f59e2e6faa42e5b1057449b58f2e
sunsplat <heeycutie@yahoo.com>
2015-11-03 12:50:51 +0800

    removed pagescroll as it's not working

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,9 @@
-<div class="container footer">
-        	<em>&copy;</em> 2015 Ellen Sun | <?php $date = new DateTime();
-              echo date_format($date, 'Y M D H:j'); ?>
-</div>
+			<div class="container footer">
+			        	<em>&copy;</em> 2015 Ellen Sun | <?php $date = new DateTime();
+			              echo date_format($date, 'Y M D H:jj'); ?>
+			</div>
 
         <script src="<?php echo base_url('assets/bootstrap/js/jquery-1.11.3.min.js'); ?>"></script>	
-        <script src="<?php echo base_url('assets/bootstrap/js/pagescroll/jquery.easing.min.js'); ?>"></script>
-        <script src="<?php echo base_url('assets/bootstrap/js/pagescroll/scrolling-nav.js'); ?>"></script>
         <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
   </body>
 </html>
