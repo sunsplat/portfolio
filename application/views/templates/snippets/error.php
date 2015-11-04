6bf8b667abf8e5923fbe7f528e4ff6b64321e361
sunsplat <heeycutie@yahoo.com>
2015-11-04 10:30:07 +0800

    adding error.php page

---
+++ application/views/templates/snippets/error.php
@@ -0,0 +1,11 @@
+<section class="error">
+	<div class="container">
+		<div class="row">
+			<div>
+				<h1 class="text-center">SORRY</h1>
+					<p>The form did not submit correctly. <br>
+					Make sure you fill out all the fields</p>
+			</div>
+		</div>
+	</div>
+</section>
