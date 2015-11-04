c14375bfe373443100c544e233b1549adc7de9fa
sunsplat <heeycutie@yahoo.com>
2015-11-04 10:30:33 +0800

    cleaned up footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,5 +5,6 @@
 
         <script src="<?php echo base_url('assets/bootstrap/js/jquery-1.11.3.min.js'); ?>"></script>	
         <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
   </body>
 </html>
