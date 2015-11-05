da419200da7cd2f9d2e892f6914d4c42dcc90202
sunsplat <heeycutie@yahoo.com>
2015-11-05 13:08:13 +0800

    3 thumbnails instead of 4

--- application/views/templates/snippets/thumbnails.php
+++ application/views/templates/snippets/thumbnails.php
@@ -6,24 +6,19 @@
 	</div>
 
 	<div class="row">
-	  <div class="col-xs-6 col-md-3">
+	  <div class="col-xs-6 col-md-4">
 	    <a href="https://stormy-ridge-1166.herokuapp.com/" class="thumbnail">
 	      <img class="thumbs" src="<?php echo base_url('assets/images/hapsit.jpg') ?>" alt="thumb">
 	    </a>
 	    <a class="center" href="https://github.com/sunsplat/simplecode_saas">Source Code</a>
 	  </div>
-	  <div class="col-xs-6 col-md-3">
+	  <div class="col-xs-6 col-md-4">
 	    <a href="https://tts2.herokuapp.com/" class="thumbnail">
 	      <img class="thumbs" src="<?php echo base_url('assets/images/movielist.jpg') ?>" alt="...">
 	    </a>
 	    <a class="center" href="https://github.com/sunsplat/tts">Source Code</a>
 	  </div>
-	  <div class="col-xs-6 col-md-3">
-	    <a href="#" class="thumbnail">
-	      <img src="<?php echo base_url('assets/images/thumb192x200.png') ?>" alt="...">
-	    </a>
-	  </div>
-	  <div class="col-xs-6 col-md-3">
+	  <div class="col-xs-6 col-md-4">
 	    <a href="#" class="thumbnail">
 	      <img src="<?php echo base_url('assets/images/thumb192x200.png') ?>" alt="...">
 	    </a>
