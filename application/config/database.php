7ff28b2a84b975ab8984c8f4aa49e6456094caa9
sunsplat <heeycutie@yahoo.com>
2015-11-17 09:12:04 +0800

    changed database to Postgre

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'test',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
