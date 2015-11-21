7764cd0fd19b3610c94490076989f8477aade98a
sunsplat <heeycutie@yahoo.com>
2015-11-22 00:25:21 +0800

    created new js folder

---
+++ assets/js/pagescroll/scrolling-nav.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('a[href^="view#"]').bind('click.page-scroll', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        console.log(target);
+
+    $('html, body').stop().animate({
+            'scrollTop': $(target).offset().top
+        }, 1500, 'easeInOutExpo', function()
+        {
+            window.location.hash = target;
+        });
+
+    });
+});
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {
+//     $('a.page-scroll').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             'scrollTop': $($('a').attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     })
+// });
+
+// });
