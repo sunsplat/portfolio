4482f60ee631acd3e9e90dbaf1a1333348388ba5
sunsplat <heeycutie@yahoo.com>
2015-11-22 00:26:31 +0800

    added paragraph class

--- application/views/templates/snippets/main.php
+++ application/views/templates/snippets/main.php
@@ -2,7 +2,7 @@
 	<div class="col-md-8 col-md-offset-2">
 		<div class="section">
 			<h2>About Me</h2>
-			<p> While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the
+			<p class="aboutme"> While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the
 				developers within my team for their ability to create beautiful, user-friendly applications. After sampling some online courses with Codecademy and Code School, I fell in love and decided to change my career path, actively pursuing web development full-time.  With my ambition, perseverance, and dedication, I have become proficient in HTML and CSS; knowledgeable in Ruby and Ruby on Rails; and comfortable with JavaScript, jQuery, PHP, MySQL and the LAMP stack in MVC frameworks. </p>
 
 				<p>Fully immersing myself in the Atlanta tech community, I regularly attend Meetups to learn new technologies and connect with others with similar passions. I have been able to use what I have learned to help other self-taught developers succeed and regularly lead the Atlanta group of FreeCodeCampers, an online full-stack bootcamp.</p>
