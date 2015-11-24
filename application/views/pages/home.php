421e3a61f244a42c0e9bea67b8e92a07f11e09c7
sunsplat <heeycutie@yahoo.com>
2015-11-24 14:35:09 +0800

    added name and title

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,6 +1,9 @@
 <div class="wrapper">
 		<section class="module parallax parallax-1" id="home">
 			      <div class="container center social">
+
+							<h1 class="name">ELLEN SUN</h1>
+							<p class="title">web developer</p>
 			      	<h1><strong>ENGAGE</strong></h1>
 		          <a href="http://www.twitter.com/ms_ellerz"><i class="fa fa-twitter fa-5x"></i></a>
 		          <a href="http://www.github.com/sunsplat"><i class="fa fa-github fa-5x"></i></a>
