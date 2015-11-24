9499a1d338c02f9e1cbb58d3f16905e98b05c047
sunsplat <heeycutie@yahoo.com>
2015-11-24 13:24:03 +0800

    added get_portfolio and set_portfolio methods to model

--- application/models/Pages_model.php
+++ application/models/Pages_model.php
@@ -5,24 +5,40 @@ class Pages_model extends CI_Model
     public function __construct()
     {
         $this->load->database();
+        $this->load->helper('url');
     }
 
     /** For retrieving portfolio elements from database.
 		 *
 		 * @since November 21, 2015
 		 */
-    public function get_portfolio($slug = FALSE)
+    public function get_portfolio($link = FALSE)
 		{
-	    	if ($slug === FALSE)
-        	{
-                $query = $this->db->get('portfolio');
-                return $query->result_array();
-        	}
+	    	if ($link === FALSE) 
+	    	{
+	    		$query = $this->db->get('portfolio');
+	    		return $query->result_array();
+	    	}
 
-	        $query = $this->db->get_where('portfolio', array('slug' => $slug));
-	        return $query->row_array();
+	    	$query = $this->db->get_where('portfolio', array('link' => $link));
+	    	return $query->row_array();
+        
 		}
 
+		public function set_portfolio()
+		{
+			$link = url_title($this->input->post('link'));
+
+			$data = array(
+				'project_name' => $this->input->post('project_name'),
+				'link' => $link,
+				'source' => $this->input->post('source'),
+				'image_url' => $img_url->input->post('image_url'),
+				'thumbnail' => $this->input->post('thumbnail')
+				);
+
+			return $this->db->insert('portfolio', $data);
+		}
 
 		/** For saving the contact info into the database
 		 *
@@ -30,7 +46,6 @@ class Pages_model extends CI_Model
 		 */
 		public function set_contact($message)
 		{
-		    $this->load->helper('url');
 
 		    $this->db->insert('contact', $message);
 		}
