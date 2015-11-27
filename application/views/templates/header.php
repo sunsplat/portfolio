e0aa755435eae8e77efe8cfe4bb62ef722ce6729
sunsplat <heeycutie@yahoo.com>
2015-11-27 13:21:42 +0800

    adding meta tag

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
     
     <title><?php echo ucwords($title); ?></title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   
   <body data-spy="scroll" data-target="navbar-fixed-top">
