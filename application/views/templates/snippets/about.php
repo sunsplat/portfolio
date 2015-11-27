53afad5e8f2a89beeb337a858900edd9437f4c6a
sunsplat <heeycutie@yahoo.com>
2015-11-27 13:22:37 +0800

    changed headings

--- application/views/templates/snippets/about.php
+++ application/views/templates/snippets/about.php
@@ -2,6 +2,7 @@
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3">
 				<h2>ABOUT ME</h2>
+
 				<p> While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the
 				developers within my team for their ability to create beautiful, user-friendly applications. After sampling some online courses with Codecademy and Code School, I fell in love and decided to change my career path, actively pursuing web development full-time.  With my ambition, perseverance, and dedication, I have become proficient in HTML and CSS; knowledgeable in Ruby and Ruby on Rails; and comfortable with JavaScript, jQuery, PHP, MySQL and the LAMP stack in MVC frameworks. </p>
 
@@ -9,8 +10,8 @@
 
 				<p>Bringing my ambition and passion for knowledge, my past professional work experience in team-oriented environments, and my adaptability gained from working abroad in international business settings, I am ready to contribute to an engineering team right away. </p>
 
-				<p>Github: http://github.com/sunsplat<br>
-				Twitter: @ms_ellerz</p>
+				<p>Github: <a href="http://github.com/sunsplat">github.com/sunsplat</a><br>
+				Twitter: <a href="http://www.twitter.com/ms_ellerz">@ms_ellerz</a></p>
 			</div>
 		</div>
 	</div>
