7245a0ba3a69a83e1091d2c57bd2ed48f1b19695
sunsplat <heeycutie@yahoo.com>
2015-11-27 14:04:28 +0800

    added meta tags

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,13 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
   <head>
-
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
     
     <title><?php echo ucwords($title); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
   </head>
   
   <body data-spy="scroll" data-target="navbar-fixed-top">
