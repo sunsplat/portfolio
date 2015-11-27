d4cbb08a4639548867d49019e5677c6120057370
sunsplat <heeycutie@yahoo.com>
2015-11-28 01:46:32 +0800

    adding second row

--- application/views/templates/snippets/about.php
+++ application/views/templates/snippets/about.php
@@ -1,17 +1,21 @@
 	<div class="container content">
-		<div class="row">
-			<div class="col-md-6 col-md-offset-3">
+		<div class="row center">
+			<div class="col-md-6 col-md-offset-3 padBottom">
 				<h2>ABOUT ME</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
 
-				<p> While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the
-				developers within my team for their ability to create beautiful, user-friendly applications. After sampling some online courses with Codecademy and Code School, I fell in love and decided to change my career path, actively pursuing web development full-time.  With my ambition, perseverance, and dedication, I have become proficient in HTML and CSS; knowledgeable in Ruby and Ruby on Rails; and comfortable with JavaScript, jQuery, PHP, MySQL and the LAMP stack in MVC frameworks. </p>
+				<p>While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the developers within my team for their ability to create beautiful, user-friendly applications. After sampling some online courses with Codecademy and Code School, I fell in love and decided to change my career path, actively pursuing web development full-time.  With my ambition, perseverance, and dedication, I have become an expert in front-end development; experienced in Ruby and Ruby on Rails; and proficient with JavaScript, jQuery, PHP, MySQL and the LAMP stack in MVC frameworks. </p>
 
 				<p>Fully immersing myself in the Atlanta tech community, I regularly attend Meetups to learn new technologies and connect with others with similar passions. I have been able to use what I have learned to help other self-taught developers succeed and regularly lead the Atlanta group of FreeCodeCampers, an online full-stack bootcamp.</p>
 
 				<p>Bringing my ambition and passion for knowledge, my past professional work experience in team-oriented environments, and my adaptability gained from working abroad in international business settings, I am ready to contribute to an engineering team right away. </p>
 
-				<p class="about">Github: <a href="http://github.com/sunsplat">github.com/sunsplat</a><br>
+				<p>Github: <a href="http://github.com/sunsplat">github.com/sunsplat</a><br>
 				Twitter: <a href="http://www.twitter.com/ms_ellerz">@ms_ellerz</a></p>
+
 			</div>
 		</div>
 	</div>
