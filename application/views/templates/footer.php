62072aeb02f25a6a46397b00ee58621c27c01e6f
sunsplat <heeycutie@yahoo.com>
2015-12-05 14:03:43 +0800

    trying to highlight required fields

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -11,6 +11,27 @@
         <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
         <script src="<?php echo base_url('assets/js/pagescroll/jquery.easing.min.js'); ?>"></script>	
         <script src="<?php echo base_url('assets/js/pagescroll/scrolling-nav.js'); ?>"></script>	       
+        <script type="text/javascript">
+	        $('.model-body').on('click', '#submit', function(e) {
+	        				if (!isFormValid()) {
+	        						e.preventDefault;
+	        						alert("Please fill in all highlighted fields!");
+	        				}
+	        		});
+        	function isFormValid() {
+		        	var isFormValid = true;
+		        	$(".required").each(function() {
+		        			if ($.trim($(this).val()).length == 0) {
+		        					$(this).addClass("has-error");
+		        					isFormValid = false;
+		        			} else {
+		        					$(this).removeClass("has-error");
+		        			}
+
+		        	return isFormValid;
+        			});
+		      };
+        </script>
 
   </body>
 </html>
