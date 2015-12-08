efe11371f2afb4d591225c77786b79f28f8fc9e8
sunsplat <heeycutie@yahoo.com>
2015-12-09 07:21:50 +0800

    for heroku

--- application/config/config.php
+++ application/config/config.php
@@ -18,8 +18,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-// $config['base_url'] = 'https://ellensun.herokuapp.com/';
-$config['base_url'] = 'http://localhost/codeigniter';
+$config['base_url'] = 'https://ellensun.herokuapp.com/';
+// $config['base_url'] = 'http://localhost/codeigniter';
 
 /*
 |--------------------------------------------------------------------------
